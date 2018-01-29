307422728f7c0041cf02e5dab9238e302b83ce50
Taariq Levack <taariq.levack@payd.co>
2018-01-29 15:28:37 +0200

    add beneficiary

--- contracts/NGO.sol
+++ contracts/NGO.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.4.18;
 
 contract NGO {
 
+    struct Beneficiary {
+        address beneficiaryAddress;
+        string id; //hashed id/passport
+        uint tokenBalance; //current token balance -- TODO consider getting from blockchain account
+    }
+
     struct Merchant {
         address merchantAddress; 
         string name;
@@ -12,13 +18,26 @@ contract NGO {
     }
 
     mapping (address => Merchant) merchants;
+    mapping (address => Beneficiary) beneficiaries;
     uint tokens;
 
     // constructor to initialize number of tokens
     function NGO(uint numberOfTokens) public {
         tokens = numberOfTokens;
     }
+
+    // finds and returns beneficiaries by address
+    function findBeneficiary(address beneficiary) public view returns (address, string, uint) {
+        return (beneficiaries[beneficiary].beneficiaryAddress, beneficiaries[beneficiary].id, 
+            beneficiaries[beneficiary].tokenBalance);
+    }
     
+    function addBeneficiary(address beneficiary, string id) public {
+        require(beneficiaries[beneficiary].beneficiaryAddress == address(0)); 
+        beneficiaries[beneficiary] = Beneficiary(beneficiary, id, 0);
+    }
+
+
     // finds and returns if a Merchant exists with the provided address
     function findMerchant(address merchant) public view returns (address, string, string, uint) {
         return (merchants[merchant].merchantAddress, merchants[merchant].name, 
@@ -27,7 +46,7 @@ contract NGO {
 
     // adds a merchant if it does not exist
     function addMerchant(address merchant, string name, string typeOfGoods) public {
-        // require(findMerchant(merchant).merchantAddress == address(0)); 
+        require(merchants[merchant].merchantAddress == address(0)); 
         merchants[msg.sender] = Merchant(merchant, name, typeOfGoods, 0);
     }
 
