206d021c1ac2e0991859d075857b262d5efe8786
AvinashSingh <tashan.avi@gmail.com>
2018-01-29 16:30:22 +0200

    removed merge conflictr

--- test/TestMerchant.js
+++ test/TestMerchant.js
@@ -4,7 +4,7 @@ const assertRevert = require('./helpers/assertRevert')
 
 contract('NGO', function (accounts) {
     let ngo = null;
-    beforeEach(async () => {
+    before(async () => {
         ngo = await NGO.new(100000);
     })
 
@@ -27,6 +27,17 @@ contract('NGO', function (accounts) {
             assert.equal(merchant[1], "", "should not have a name");
             assert.equal(merchant[2], "", "should not have types of goods");
             assert.equal(merchant[3], 0, "should not have token balance");
+        })
+    });
+
+    describe('Testing Merchants', () => {
+        it('should revert if not a duplicate merchant', async () => {
+            await ngo.addMerchant("0x627306090abaB3A6e1400e9345bC60c78a8BEf57", "BobSupplier", "Water;Apples;rice");
+            const merchant = await ngo.findMerchant("0x627306090abaB3A6e1400e9345bC60c78a8BEf57");
+            assert.equal(merchant[0], "0x627306090abaB3A6e1400e9345bC60c78a8BEf57".toLowerCase(), "address does not match of added merchant");
+            assert.equal(merchant[1], "BobSupplier", "type of goods does not match of added merchant");
+            assert.equal(merchant[2], "Water;Apples;rice", "type of goods does not match of added merchant");
+            assert.equal(merchant[3], 0, "token balance does not match of added merchant 0");
         })
     });
     
--- test/helpers/assertRevert.js
+++ test/helpers/assertRevert.js
@@ -1,5 +1,4 @@
 module.exports = async promise => {
-<<<<<<< HEAD
   try {
     await promise
     assert.fail('Expected revert not received')
@@ -8,13 +7,3 @@ module.exports = async promise => {
     assert(revertOrAssertFound, `Expected "revert", got ${error} instead`)
   }
 }
-=======
-    try {
-      await promise
-      assert.fail('Expected revert not received')
-    } catch (error) {
-      const revertOrAssertFound = error.message.search('revert') >= 0 || error.message.search('assert') >= 0
-      assert(revertOrAssertFound, `Expected "revert", got ${error} instead`)
-    }
-  }
->>>>>>> origin/feature/11_beneficiary_crud
